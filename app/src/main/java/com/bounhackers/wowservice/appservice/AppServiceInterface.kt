e12bf3e185655abe76e1b44eba74d134049a6431
Mahmut Karaca <mahmutkaraca95@gmail.com>
2018-12-01 21:24:35 +0300

    add login screen

--- app/src/main/java/com/bounhackers/wowservice/appservice/AppServiceInterface.kt
+++ app/src/main/java/com/bounhackers/wowservice/appservice/AppServiceInterface.kt
@@ -3,10 +3,16 @@ package com.bounhackers.wowservice.appservice
 import com.bounhackers.wowservice.Constants
 import com.bounhackers.wowservice.appservice.model.LoginModel
 import io.reactivex.Observable
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.POST
+import java.net.CookieHandler
+import java.net.CookieManager
+import java.util.concurrent.TimeUnit
 
 interface AppServiceInterface {
 
@@ -15,10 +21,23 @@ interface AppServiceInterface {
 
     companion object Factory {
         fun create(): AppServiceInterface {
-            val retrofit = retrofit2.Retrofit.Builder()
+            val logging = HttpLoggingInterceptor()
+            logging.level = HttpLoggingInterceptor.Level.BODY
+
+            val handler: CookieHandler = CookieManager()
+            val client: OkHttpClient = OkHttpClient.Builder()
+                .addNetworkInterceptor(logging)
+                .cookieJar(JavaNetCookieJar(handler))
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .build()
+
+            val retrofit = Retrofit.Builder()
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
                 .baseUrl(Constants.BASE_URL)
+                .client(client)
                 .build()
             return retrofit.create(AppServiceInterface::class.java)
         }
--- app/src/main/java/com/bounhackers/wowservice/splash/SplashFragment.kt
+++ app/src/main/java/com/bounhackers/wowservice/splash/SplashFragment.kt
@@ -1,6 +1,7 @@
 package com.bounhackers.wowservice.splash
 
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -8,16 +9,53 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.bounhackers.wowservice.R
+import io.reactivex.Observable
+import io.reactivex.disposables.Disposable
+import java.util.concurrent.TimeUnit
 
 class SplashFragment : Fragment() {
+
+    private var onSplashTimerEndListener: OnSplashTimerEndListener? = null
+    private var timerDisp: Disposable? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_splash, container, false)
     }
 
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        if(context is OnSplashTimerEndListener) {
+            this.onSplashTimerEndListener = context
+        } else {
+            throw RuntimeException("Dude! Activity'e OnSplashTimerEndListener ekler misin? Tesekkurler...")
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        this.timerDisp = Observable.timer(1, TimeUnit.SECONDS)
+            .subscribe {
+                this.onSplashTimerEndListener?.onSplashTimerEnd()
+            }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        this.timerDisp?.dispose()
+    }
+
+    interface OnSplashTimerEndListener {
+        fun onSplashTimerEnd()
+    }
+
+    companion object {
+        fun newInstance(): SplashFragment {
+            return SplashFragment()
+        }
+    }
 
 }
